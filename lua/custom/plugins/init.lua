bf8b9bf25c4447cb4b1df78b1e91d45b3d9494c9
JW <wongjianwen0520@gmail.com>
2025-12-02 00:02:17 +0800

    fix: error on focus terminal and close, feat: remove nvim-tree and terminal on startup

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -13,17 +13,14 @@ return {
     },
     config = function(_, opts)
       -- 1. SETUP NVIM-TREE ONCE (Globally)
-      -- We configure it here to be "dumb" so the drawer can control it.
       require('nvim-tree').setup {
         view = {
           side = 'right',
-          width = 25, -- Match your drawer size
-          preserve_window_proportions = true, -- Helps prevent misalignment
+          width = 25,
+          preserve_window_proportions = true,
         },
         actions = {
-          open_file = {
-            resize_window = false,
-          },
+          open_file = { resize_window = false },
         },
         hijack_directories = {
           enable = false,
@@ -45,7 +42,9 @@ return {
         should_close_on_bufwipeout = false,
 
         on_vim_enter = function(event)
-          event.instance.open { focus = false }
+          -- [FIX 1] Commented this out so it doesn't open on startup
+          -- event.instance.open { focus = false }
+
           -- Toggle Keymap
           vim.keymap.set('n', '<A-n>', function()
             event.instance.focus_or_toggle()
@@ -53,16 +52,13 @@ return {
         end,
 
         on_did_create_buffer = function()
-          -- JUST open the tree. Do not run setup() here.
           require('nvim-tree.api').tree.open { current_window = true }
         end,
 
         on_did_open = function(ctx)
           local api = require 'nvim-tree.api'
           api.tree.reload()
-
           vim.api.nvim_win_set_width(ctx.winid, 25)
-
           vim.opt_local.number = false
           vim.opt_local.relativenumber = false
           vim.opt_local.signcolumn = 'no'
@@ -86,25 +82,40 @@ return {
         end,
 
         on_vim_enter = function(event)
-          event.instance.open { focus = false }
+          -- [FIX 1] Commented this out so it doesn't open on startup
+          -- event.instance.open { focus = false }
 
-          -- Keymaps
+          -- Normal Mode Toggle
           vim.keymap.set('n', '<A-h>', function()
             event.instance.focus_or_toggle()
           end, { desc = 'Toggle Terminal' })
-          -- Ensure this works in terminal mode too so you can toggle it closed while typing
-          vim.keymap.set('t', '<A-h>', [[<C-\><C-n><cmd>lua require('nvim-drawer').get_drawer(2):focus_or_toggle()<CR>]])
 
-          -- Terminal Controls (The "t" group)
+          -- [FIX 2] Terminal Mode Toggle
+          -- We use a Lua function so we can use 'event.instance' directly
+          vim.keymap.set('t', '<A-h>', function()
+            -- 1. Exit Terminal Mode (equivalent to <C-\><C-n>)
+            vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<C-\\><C-n>', true, false, true), 'n', false)
+
+            -- 2. Toggle the specific drawer instance we are already configured for
+            -- We wrap this in schedule to ensure the mode switch happens first
+            vim.schedule(function()
+              event.instance.focus_or_toggle()
+            end)
+          end)
+
+          -- Terminal Controls
           vim.keymap.set('n', '<leader>tt', function()
             event.instance.open { mode = 'new' }
           end, { desc = '[N]ew Terminal' })
+
           vim.keymap.set('n', '<leader>tn', function()
             event.instance.go(1)
           end, { desc = 'Next [T]erminal' })
+
           vim.keymap.set('n', '<leader>tp', function()
             event.instance.go(-1)
           end, { desc = 'Previous [T]erminal' })
+
           vim.keymap.set('n', '<leader>tz', function()
             event.instance.toggle_zoom()
           end, { desc = '[Z]oom Terminal' })
