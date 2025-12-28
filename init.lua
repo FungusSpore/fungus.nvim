9c0b565ebc7ac16c14b98e093cf9d8e06cd4d5c4
JW <wongjianwen0520@gmail.com>
2025-12-29 00:33:55 +0800

    feat: rust LSP

--- init.lua
+++ init.lua
@@ -499,7 +499,7 @@ require('lazy').setup({
         clangd = {},
         -- gopls = {},
         pyright = {},
-        rust_analyzer = {},
+        -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
@@ -541,6 +541,7 @@ require('lazy').setup({
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
+        'rust-analyzer',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -3,6 +3,17 @@
 --
 -- See the kickstart.nvim README for more information
 return {
+  {
+    'mrcjkb/rustaceanvim',
+    version = '^5', -- Recommended
+    lazy = false, -- This plugin is already lazy
+    config = function()
+      local bufnr = vim.api.nvim_get_current_buf()
+      vim.keymap.set('n', '<leader>a', function()
+        vim.cmd.RustLsp 'codeAction'
+      end, { silent = true, buffer = bufnr })
+    end,
+  },
   { -- File Explorer & Drawer Wrapper
     'nvim-tree/nvim-tree.lua',
     version = '*',
